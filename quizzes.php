e751ce76a1942a3277979bc06dc13561879bb627
Gaurav <ggmathur@gmail.com>
2011-12-03 17:32:59 -0500

    Quick bug fix.

--- quizzes.php
+++ quizzes.php
@@ -51,7 +51,7 @@
   function setActivateQuiz($quizID, $activate) {
     $dbQuery = sprintf("UPDATE quizzes SET active='%s' WHERE id='%s'",
         mysql_real_escape_string($activate));
-        mysql_real_escape_string($userId));
+        mysql_real_escape_string($quizID));
     if (is_set(getDBResultAffected($dbQuery))) {
         echo json_encode(array('success'=> true))
     }
