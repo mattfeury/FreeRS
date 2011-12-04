ba287f2593586b9593267476d3ecf7f98d720d7d
Matt Feury <mattfeury@gmail.com>
2011-12-03 22:33:51 -0500

    Avoid double echo

--- questions.php
+++ questions.php
@@ -2,25 +2,21 @@
 	include 'db_helper.php';
 
   function addQuestion($quizID, $numChoices, $correctChoice) {
-	$dbQuery = sprintf("INSERT INTO questions (quiz_id, num_choices, correct_choice) VALUES (%d, %d, %d)",
-      ($quizID),
-      ($numChoices),
-      ($correctChoice));
+    $dbQuery = sprintf("INSERT INTO questions (quiz_id, num_choices, correct_choice) VALUES (%d, %d, %d)",
+        ($quizID),
+        ($numChoices),
+        ($correctChoice));
 
-	$result = getDBResultInserted($dbQuery, 'id');
-	header("Content-type: application/json");
-	echo json_encode($result); 
+    $result = getDBResultInserted($dbQuery, 'id');
+    header("Content-type: application/json");
 
-	$dbQuery = sprintf("UPDATE quizzes SET current_question_id=%d WHERE id=%d",
-		($result['id']),
-		($quizID)
-		);
+    $dbQuery = sprintf("UPDATE quizzes SET current_question_id=%d WHERE id=%d",
+      ($result['id']),
+      ($quizID)
+      );
 
     if (getDBResultAffected($dbQuery) > 0 ) {
-		echo json_encode($result); 
-    }
-    else {
-
+      echo json_encode($result); 
     }
   }
 
